d80643b9f3f5c5b3a6b1aa82cd67ad104fe37fdd
Joseph Anselm <iamjoeanselm@gmail.com>
2024-03-10 17:47:45 +0100

    About us page fix

--- components/About-us/SectionC.js
+++ components/About-us/SectionC.js
@@ -3,20 +3,20 @@ import React from 'react';
 
 const SectionC = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center">
+    <div className="container mx-auto flex flex-col md:flex-row items-center relative">
       {/* First Column */}
       <div className="relative w-full md:w-1/2 overflow-hidden">
         <img
-          src="/images/excella-img2.jpg"  // Replace with your image URL
+          src="/images/excella-img1.jpg"  // Replace with your image URL
           alt="First Image"
           className="w-full h-auto rounded-3xl"
         />
       </div>
 
       {/* Second Column (Overlapping) */}
-      <div className="relative w-full md:w-1/2 md:-mt-16 overflow-hidden">
+      <div className="absolute top-1/2 md:-mt-1/2 md:left-full md:transform md:-translate-x-1/2 md:w-1/2 overflow-hidden">
         <img
-          src="/images/excella-img1.jpg"  // Replace with your image URL
+          src="/images/excella-img2.jpg"  // Replace with your image URL
           alt="Second Image"
           className="w-full h-auto rounded-full mx-auto"
         />
@@ -26,3 +26,4 @@ const SectionC = () => {
 };
 
 export default SectionC;
+
