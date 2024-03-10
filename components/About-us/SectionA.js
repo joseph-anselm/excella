e881b93396bccf009fbbb00f6d8b346324268383
Joseph Anselm <iamjoeanselm@gmail.com>
2024-03-10 09:32:13 +0100

    About us page fix

--- components/About-us/SectionA.js
+++ components/About-us/SectionA.js
@@ -24,17 +24,15 @@ const SectionA = () => {
 
         {/* Column 2 */}
         <div className="md:w-1/3 p-6">
-          <p className="text-lg md:text-xl">
-            This is a responsive paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          <p className="text-sm md:text-lg">
+          Excella Energy Services Ltd is a leading indigenous oil servicing company based in Port Harcourt, Nigeria. Our operational expertise is strategically designed to provide premier services to the dynamic oil and gas sector. Specializing in a diverse range of services such as brine filtration, wellbore cleaning, and the supply of cutting-edge tools and equipment, we are committed to delivering superior results.
           </p>
         </div>
 
         {/* Column 3 */}
         <div className="md:w-1/3 p-6">
-          <p className="text-lg md:text-xl">
-            Another responsive paragraph. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat.
+          <p className="text-sm md:text-lg">
+          As a dedicated partner in the Oil & Gas industry, Excella Energy Services Ltd prioritizes excellence, reliability, and innovation. Our unwavering commitment to providing top-tier solutions distinguishes us as a trusted service provider in the Nigerian energy landscape. For unparalleled oil and gas field services, choose Excella Energy Services Ltd - your trusted partner for excellence in the industry.
           </p>
         </div>
       </div>
