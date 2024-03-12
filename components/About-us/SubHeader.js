34e11858893fe5c72bf9467c6b0df9fd192cfa5c
Joseph Anselm <iamjoeanselm@gmail.com>
2024-03-12 04:04:58 +0100

    what we do page fix

--- components/About-us/SubHeader.js
+++ components/About-us/SubHeader.js
@@ -1,5 +1,3 @@
-// components/AboutUsPage.js
-
 import React from 'react';
 
 const AboutUsPage = () => {
@@ -7,20 +5,25 @@ const AboutUsPage = () => {
     <div>
       {/* Header */}
       <div
-        className="relative h-[400px] max-h-[400px] bg-cover bg-center"
+        className="relative h-[400px] max-h-[400px] bg-cover bg-center relative"
         style={{ backgroundImage: `url('/images/excella-img3.jpg')` }}
       >
-        <div className="absolute inset-0 bg-black opacity-50"></div>
+        {/* Image Overlay */}
+        <div className="absolute inset-0 bg-black opacity-60"></div>
+
+        {/* Title */}
         <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-white text-5xl font-bold uppercase z-10 text-center">
-            WHO WE ARE
+          <h1 className="text-white text-5xl font-extrabold uppercase z-10 text-center">
+            <span className="text-[#7FB000]">WHO</span> WE <span className="text-[#7FB000]">ARE</span>
           </h1>
         </div>
       </div>
 
-      
+      {/* Rest of the page content */}
+      {/* Add your content here */}
     </div>
   );
 };
 
 export default AboutUsPage;
+
